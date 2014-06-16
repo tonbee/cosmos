c756f3ba9b9068918ca17926fb528df23682d1af
tonbee <stylohouse@yahoo.co.jp>
2014-06-17 06:06:32 +0900

    太陽軌道-計算だけ

--- start1.js
+++ start1.js
@@ -23,15 +23,6 @@ var enkei_num = enkei_haba * -1 / 2 ;//遠景画像の表示pos 元画像の-1/2
 
 var sora_step = 0;//空の色変化のステップ数
 
-    var sora_colorcode = [
-        '#E0F2F7','#CEECF5','#A9E2F3','#81DAF5','#58D3F7',
-        '#2ECCFA','#00BFFF','#01A9DB','#0174DF','#013ADF',
-        '#045FB4','#DF7401','#B43104','#2A0A1B','#2A0A22',
-        '#220A29','#120A2A','#0A122A','#0A2229','#0B2F3A',
-        '#1C1C1C','#424242','#848484'       
-    ];
-
-
 
 function test2()//メインループ
 {
@@ -57,28 +48,17 @@ function sora_iro()
 空色を配列に格納するやり方では不自然な感じする。
 
 R255:G255:B255
-R0:G255:B255
-R0:G0:B255
-R0:G0:B50
-R80:G80:B80
+R20:G255:B255
+R20:G20:B255
+R20:G20:B20
 R100:G100:B100
 R255:G255:B255
-
-time
-1   255 255 255
-236  20   255  255
-
-237   20  254  255
-471   20   20  255
-
-472   20  20   254
-706   20  20   20
-
-707  100 100 100
-862   255  255  255
-
-
 の順でカラーコードを遷移させれば、空の色が滑らかに変化させれるのでないか
+
+※.toString(16)で10進数を16進数に変換するのだが、
+このとき対象となる10進数が16以下だと8などとなってしまい、
+カラーコードとしての用途である08にならない。
+ので、RGB値の下限を0ではなく20にする
 
 sora_colorR,sora_colorG,sora_colorBで表現する。
 sora_colorR.toString(16)で16進数に変える
@@ -86,48 +66,101 @@ sora_colorR.toString(16)で16進数に変える
 
 */
 
-
-	sora_step = sora_step + 1;
-
+sora_step = sora_step + 1;
+//朝～午前
 if( 1 <= sora_step && sora_step <= 236)
 {   sora_colorR = sora_colorR - 1;}
-
   
-  if( 237 <= sora_step  && sora_step <= 471)
+//日中  
+if( 237 <= sora_step  && sora_step <= 471)
 {   sora_colorG = sora_colorG - 1;}
-  
-    if( 472 <= sora_step  && sora_step <= 706)
+
+//午後～夜
+if( 472 <= sora_step  && sora_step <= 706)
 {   sora_colorB = sora_colorB - 1;}
   
-
-
-      if( 707 <= sora_step  && sora_step <= 800)
-{       sora_colorR = sora_colorR + 2;
+//夜～明け方  
+if( 707 <= sora_step  && sora_step <= 800)
+{    sora_colorR = sora_colorR + 2;
      sora_colorG = sora_colorG + 2;   
      sora_colorB = sora_colorB + 2; 
-
      }
      
-           if( 801 <= sora_step  && sora_step <= 811)
-{       sora_colorR = sora_colorR + 5;
+//夜明け間際     
+if( 801 <= sora_step  && sora_step <= 811)
+{    sora_colorR = sora_colorR + 5;
      sora_colorG = sora_colorG +5;   
      sora_colorB = sora_colorB + 5; 
-
      }
      
-     
-     
-         if(sora_step >=812)
-{           sora_colorR = 255;
+if(sora_step >=812)
+{   sora_colorR = 255;
     sora_colorG = 255;
     sora_colorB = 255;
     sora_step = 0;
     } 
     
-console.log(sora_step,sora_colorR,sora_colorB,sora_colorG);
+/*
+このことから、太陽は1で日の出、600で日没くらいのスパンで動くと予想される。
+仮の太陽画像では、画像サイズに対して太陽の中心座標が、ｘ165、ｙ120
+辺りにある。これは、フォトショップの情報ボックスで分かる。
 
-	
-	
+1）初期配置で太陽の円が地平線にくるように調節する。
+
+仮の処置として、一旦、遠景画像を非表示にする。
+htmlのCSS部分でtopを0から1000にした。
+
+2）600タイムカウント（100ミリ秒単位）で日没するような座標軌道を計算し、動かす。
+太陽仮画像の
+日出位置、x-100:y200（これは画像によって変わる）
+日没位置、x400:y200
+南中位置、x150:y-80
+
+ここからは数学だ。
+上記3点を通る2次曲線の方程式を求めなさい。
+
+y = ax^2 + bx + c 
+
+200 = 10000a - 100b + c
+200 = 160000a + 400b + c
+-80 = 22500a + 150b + c
+
+10000a - 100b = 160000a + 400b
+-500b = 150000a
+b = -300a
+
+200 - 10000a + 100b = -80 - 22500a - 150b
+280 + 12500a = -250b
+
+280 + 12500a = 75000a
+62500a = 280
+ 
+a = 280/62500 = 28/6250 = 14/3125
+b = -300*14/3125 = -60*14/625 = -12*14/125 = -168/125
+
+200 = 10000 * 14/3125 - 100 * -168/125 + c
+200 = 2000 * 14/ 625 + 20 * 168 / 25 + c
+200 = 400 * 14 / 125 + 4*168 / 5 + c
+200 = 80 * 14/25 + 672/5 + c
+c = 200 - 16*14/5 - 672/5
+= 200 - ( 224 + 672) / 5
+= 200 - 896/5 = 1000/5 - 896/5 = 104/5
+
+∴y =  14/3125x^2 -168/125x + 104/5
+
+ｘ座標は-100から+400まで動く
+タイムラインは600かけて動く
+
+ｘ座標が-100から始まって、タイムカウントが+1する度に、
+ｘ座標が+5/6されて、対応するｙが上記関数で求められる
+
+
+
+
+
+3）CSSではみでた部分をマスクする。
+
+*/
 
 var r16 = sora_colorR.toString(16);
 var g16 = sora_colorG.toString(16);
@@ -217,6 +250,16 @@ function shoki_haichi()
 	//地面（遠景）png画像の横幅取得
 	var ele = document.getElementById("img_enkei");
 	enkei_haba = ele.width;
+	
+	//太陽を初期配置
+	var ele = document.getElementById("img_taiyou");
+	ele.style.position = 'absolute'; 
+	ele.style.left = '150px'; 
+	ele.style.top  = '-80px'; 
+
+	
+	
+	
 
 }//function shoki_haichi()
 
