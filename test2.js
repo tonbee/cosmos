c80cf14ba4c9cd1e7feeaaa1995dadff687a9e31
tonbee <stylohouse@yahoo.co.jp>
2014-06-29 04:02:00 +0900

    カオスコントロール

--- test2.js
+++ test2.js
@@ -17,24 +17,25 @@
 
 */
 
-	var yoko_pls = new Array();
-	var yoko_block = new Array();	
-	var tate_block = new Array();				
+
+	var long_block = new Array();	//長辺方向へ、移動距離をブロック単位にランダム分割する
+	var short_block = new Array();	//短辺方向へ、移動距離をブロック単位にランダム分割する			
 				
-	var pls = new Array(400);  //始点終点間の距離400まで対応しますよ
-	var pls_t = new Array(400);  //始点終点間の距離400まで対応しますよ
+	var long_pleats = new Array(400);  //始点終点間の距離400まで対応しますよ
+	var short_pleats = new Array(400); 
 
 	for (var i=0; i<= 400; i++){
-		pls[i] = new Array(5);
-		pls_t[i] = new Array(5);
+		long_pleats[i] = new Array(5);  //2次元配列
+		short_pleats[i] = new Array(5);
 	}
 
 
 
 
-//ランダムボード
+//ブロックからプリーツを生成するためのランダムボード
 
-var pls0 = [
+//ブロック値0プリーツ
+var pleats0 = [
   [0, 1, 2, 3, 4],
    
   [0, 0, 0, 0, 0], 
@@ -62,8 +63,8 @@ var pls0 = [
 
 ];
 
-
-var pls1 = [
+//ブロック値1プリーツ
+var pleats1 = [
   [0, 1, 2, 3, 4],
    
   [0, 1, 1, 0, -1], 
@@ -87,8 +88,8 @@ var pls1 = [
   [0, 0, 0, 0, 1]
 ];
 
-
-var pls2 = [
+//ブロック値2プリーツ
+var pleats2 = [
   [0, 1, 2, 3, 4],
    
   [0, 1, 1, 0, 0], 
@@ -127,30 +128,21 @@ function draw_taiyou() {
     return false;
   }
   
-  	var s_x = 200;
+  	var s_x = 200;//始点座標
 	var s_y = 100;
-	var e_x = 280;
-	var e_y = 100;
+	var e_x = 280;//終点座標
+	var e_y = 130;
   
   
-  
-  var ctx = canvas.getContext('2d');
-  ctx.beginPath();
-	
-	//横80の直線があるとする	    
-	ctx.strokeStyle = 'rgba(0, 0, 0,1)'; 
-	ctx.moveTo(s_x,s_y);
-	ctx.lineTo(e_x,e_y);
-	ctx.closePath();  
-	ctx.stroke();	
+
 	
 	
 	var x_distance = Math.abs(e_x - s_x);
 	var y_distance = Math.abs(e_y - s_y);
 	
 	if( x_distance >= y_distance ){
-	var long_distance = x_distance;
-	var short_distance = y_distance;
+	var long_distance = x_distance;;//目標への長辺距離
+	var short_distance = y_distance;//目標への短辺距離
 	}
 	else{
 	var long_distance = y_distance;
@@ -159,26 +151,49 @@ function draw_taiyou() {
 	
 	
 	
-	var line_distance = 8;//目標への長辺距離
+
 	
-	distance_to_pleats(line_distance);//pls[1..8][1..4]に長辺方向のプリーツが生成された
+	distance_to_pleats(long_distance);//long_pleats[1..8][1..4]に長辺方向のプリーツが生成された
 
-
-	var tate = 5;//目標への短辺距離
-
-	pleatsnum_to_pleats(line_distance,tate);//pls_t[1..8][1..4]に短辺方向のプリーツが生成された
+	pleatsnum_to_pleats(long_distance,short_distance);//short_pleats[1..8][1..4]に短辺方向のプリーツが生成された
 	
 
+　　  
+	var ctx = canvas.getContext('2d');
+  	ctx.beginPath();
+  	
+  	
+  	for(var i = 1; i<=long_distance; i++){
+  	
+  	  	for(var j = 1; j<=4; j++){
+  	  	
+  	  	s_x = s_x + long_pleats[i][j];
+  	  	s_y = s_y + short_pleats[i][j];
+  	  	
+  	  	
+  	  		ctx.strokeStyle = 'rgba(0, 0, 0,1)'; 
+	ctx.moveTo(s_x,s_y);
+	ctx.lineTo(e_x,e_y);
+	ctx.closePath();  
+	ctx.stroke();	
+  	  	
+  	  	
+  	  	}//for j
+  	
+  	}//for i
+  	
+	
+	    
 
 
 	
   
 }//draw
 
+//以上、CANVAS記述
 
 
-
-function distance_to_pleats(line_distance)
+function distance_to_pleats(long_distance)
 {
 
 	
@@ -186,45 +201,45 @@ function distance_to_pleats(line_distance)
 	for (var i = 0;i <= 100;i++)
 	{
 
-	yoko_pls[i] = 0;  
-	yoko_block[i] = 1;
+
+	long_block[i] = 1;
 	
-	pls[i][0] = 0;
-	pls[i][1] = 0;
-	pls[i][2] = 0;
-	pls[i][3] = 0;
-	pls[i][4] = 0;
-	pls[i][5] = 0;
+	long_pleats[i][0] = 0;
+	long_pleats[i][1] = 0;
+	long_pleats[i][2] = 0;
+	long_pleats[i][3] = 0;
+	long_pleats[i][4] = 0;
+	long_pleats[i][5] = 0;
 	
-	pls_t[i][0] = 0;
-	pls_t[i][1] = 0;
-	pls_t[i][2] = 0;
-	pls_t[i][3] = 0;
-	pls_t[i][4] = 0;
-	pls_t[i][5] = 0;
+	short_pleats[i][0] = 0;
+	short_pleats[i][1] = 0;
+	short_pleats[i][2] = 0;
+	short_pleats[i][3] = 0;
+	short_pleats[i][4] = 0;
+	short_pleats[i][5] = 0;
 
 	} 
 	
 
 
-//line_distanceに対して、4割くらいの0ブロックが発生するとする。
-	var zero_block = Math.floor(line_distance * 0.4);//0ブロックの個数
+//long_distanceに対して、4割くらいの0ブロックが発生するとする。
+	var zero_block = Math.floor(long_distance * 0.4);//0ブロックの個数
 	
 	
-//line_distance個のブロック+1+2+0+2+2+0+0+1の生成
+//long_distance個のブロック+1+2+0+2+2+0+0+1の生成
 	
 	for (var i = 1;i <= zero_block;i++)
 	{
 
-		var randnum = 1 + Math.floor( Math.random() * (line_distance) );
+		var randnum = 1 + Math.floor( Math.random() * (long_distance) );
 		
-		if (yoko_block[randnum] == 1)
+		if (long_block[randnum] == 1)
 		{
 		
 
-		yoko_block[randnum] = 0;
+		long_block[randnum] = 0;
 		
-		two_block(line_distance);
+		two_block(long_distance);
 		}
 
 	} 
@@ -233,34 +248,34 @@ function distance_to_pleats(line_distance)
 	
 	//1ブロックは4回の+-から出来ている。
 
-	for ( var i = 1; i <= line_distance; i++)
+	for ( var i = 1; i <= long_distance; i++)
 	{
-		if(yoko_block[i] == 0){
+		if(long_block[i] == 0){
 			var randnum0 = 1 + Math.floor( Math.random() * 19 );
 			for( var j= 1; j <= 4; j++ ){
-				pls[i][j]=pls0[randnum0][j];
+				long_pleats[i][j]=pleats0[randnum0][j];
 				}
 			}
 
 
-		if(yoko_block[i] == 1){
+		if(long_block[i] == 1){
 			var randnum0 = 1 + Math.floor( Math.random() * 16 );	
 			for( var j= 1; j <= 4; j++ ){
-				pls[i][j]=pls1[randnum0][j];
+				long_pleats[i][j]=pleats1[randnum0][j];
 				}
 			
 		}
 
-		if(yoko_block[i] == 2){
+		if(long_block[i] == 2){
 			var randnum0 = 1 + Math.floor( Math.random() * 10 );
 			for( var j= 1; j <= 4; j++ ){
-				pls[i][j]=pls2[randnum0][j];
+				long_pleats[i][j]=pleats2[randnum0][j];
 			}
 		}
 
 
 	}//for i
-	//pls[line_distance][1..4]にプリーツが格納された
+	//long_pleats[long_distance][1..4]にプリーツが格納された
 	
 	
 }//function distance_to_pleats
@@ -269,27 +284,27 @@ function distance_to_pleats(line_distance)
 
 
 
-function pleatsnum_to_pleats(line_distance,tate)
+function pleatsnum_to_pleats(long_distance,short_distance)
 {
 
-	for(var i = 1; i <= line_distance; i++)
+	for(var i = 1; i <= long_distance; i++)
 	{
-	tate_block[i] = 0;
+	short_block[i] = 0;
 	}//for i
 	
 	
-	for(var i = 1; i <= tate; i++)
+	for(var i = 1; i <= short_distance; i++)
 	{
 	
-	var randnum = 1 + Math.floor( Math.random() * (line_distance) );
+	var randnum = 1 + Math.floor( Math.random() * (long_distance) );
 	
-	if (tate_block[randnum] < 2 ){   tate_block[randnum] = tate_block[randnum]+1;   }
+	if (short_block[randnum] < 2 ){   short_block[randnum] = short_block[randnum]+1;   }
 	else
 	{
-		for(var j = 1; j <= line_distance; j++){
+		for(var j = 1; j <= long_distance; j++){
 		
-			if(tate_block[j]  < 2 ){
-				tate_block[j] = tate_block[j] + 1;
+			if(short_block[j]  < 2 ){
+				short_block[j] = short_block[j] + 1;
 				break;
 			}//if
 		 
@@ -304,26 +319,26 @@ function pleatsnum_to_pleats(line_distance,tate)
 
 	//1ブロックは4回の+-から出来ている。
 
-	for ( var i = 1; i <= line_distance; i++)
+	for ( var i = 1; i <= long_distance; i++)
 	{
-		if(tate_block[i] == 0){
+		if(short_block[i] == 0){
 			var randnum0 = 1 + Math.floor( Math.random() * 19 );
 			for( var j= 1; j <= 4; j++ ){
-				pls_t[i][j]=pls0[randnum0][j];
+				short_pleats[i][j]=pleats0[randnum0][j];
 			}//for j
 		}
 
-		if(tate_block[i] == 1){
+		if(short_block[i] == 1){
 			var randnum0 = 1 + Math.floor( Math.random() * 16 );	
 			for( var j= 1; j <= 4; j++ ){
-				pls_t[i][j]=pls1[randnum0][j];
+				short_pleats[i][j]=pleats1[randnum0][j];
 			}//for j
 		}
 
-		if(tate_block[i] == 2){
+		if(short_block[i] == 2){
 			var randnum0 = 1 + Math.floor( Math.random() * 10 );
 			for( var j= 1; j <= 4; j++ ){
-				pls_t[i][j]=pls2[randnum0][j];
+				short_pleats[i][j]=pleats2[randnum0][j];
 			}//for j
 		}
 
@@ -351,16 +366,16 @@ function pleatsnum_to_pleats(line_distance,tate)
 
 //ブロックの+1の中からランダムに選び+2に替える
 
-		        function two_block(line_distance)
+		        function two_block(long_distance)
 		        {
-				var randnum = 1 + Math.floor( Math.random() * (line_distance ) );
-				if (yoko_block[randnum] == 1)
+				var randnum = 1 + Math.floor( Math.random() * (long_distance ) );
+				if (long_block[randnum] == 1)
 				{
-				yoko_block[randnum] = 2;
+				long_block[randnum] = 2;
 				}
 				else
 				{
-				two_block(line_distance);
+				two_block(long_distance);
 				}
 			}
 
@@ -369,7 +384,7 @@ function pleatsnum_to_pleats(line_distance,tate)
 
 
 
-//以上、CANVAS記述
+
 
 
 function test2()//メインループ
